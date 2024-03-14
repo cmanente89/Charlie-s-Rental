7ddd9ae78e168e6c7527dc0a97b4646e3dcb7899
Carlos Manente <cmanente89@gmail.com>
2024-03-14 12:14:07 -0300

    html css y js

---
+++ js/index.js
@@ -0,0 +1,41 @@
+{/* <div class="formulario">
+    <div class="tipo-bicicleta">
+        <label for="exampleSelect1" class="form-label mt-4">Seleccione el tipo de bicicleta</label>
+        <select class="form-select" id="rentalForm_bikeType">  <option>Básica</option>
+            <option>Premium</option>
+        </select>
+    </div>
+    <div class="cantidad-bicicletas">
+        <div>
+            <label class="col-form-label mt-4" for="inputDefault">Seleccion la cantidad de bicicletas</label>
+            <input type="text" class="form-control" placeholder=" " id="rentalForm_quantity">  </div>
+    </div>
+    <div class="cantidad-horas">
+        <label for="exampleSelect1" class="form-label mt-4">Seleccione la cantidad de horas</label>
+        <select class="form-select" id="rentalForm_duration">  <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+        </select>
+    </div>
+    <button type="submit" id="submitButton">Calcular</button>
+</div> */}
+
+const seleccionTipo = document.getElementById("seleccion-tipo");
+const seleccionCantidad = document.getElementById("seleccion-cantidad");
+const seleccionHoras = document.getElementById("seleccion-horas");
+const botonReservar = document.getElementById("boton-reserva");
+
+
+
+
+console.log(seleccionTipo);
+console.log(seleccionCantidad);
+console.log(seleccionHoras);
+console.log(botonReservar);
+
+
