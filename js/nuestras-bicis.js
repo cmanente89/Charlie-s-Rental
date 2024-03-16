a44c65b0e3d390b232469cb217fe0c34bae49fab
Carlos Manente <cmanente89@gmail.com>
2024-03-16 18:39:49 -0300

    mas cambios js

--- js/nuestras-bicis.js
+++ js/nuestras-bicis.js
@@ -2,11 +2,11 @@
 //USAR EL BOTON PARA MOSTRAR STOCK, deultima poner otro boton que linkee a la reserva
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Retrieve stock values from local storage
+
     const stockBasica = localStorage.getItem("stockBasica");
     const stockPremium = localStorage.getItem("stockPremium");
 
-    // Update innerHTML of the corresponding elements
+
     document.getElementById("display-stock-basica").innerText = stockBasica;
     document.getElementById("display-stock-premium").innerText = stockPremium;
 });
